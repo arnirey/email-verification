a88b703a2e38ba835b1ea7e32316da21bff4f5d5
lv31 <lordvirgo31@yahoo.com>
2018-03-03 19:35:50 +0800

    forgot password created

---
+++ mail_configuration.php
@@ -0,0 +1,12 @@
+<?php
+define("PROJECT_HOME","http://localhost/cmsc207-teamc/email-verification");
+
+define("PORT", "587"); // port number
+define("MAIL_USERNAME", "cmsc207.teamc@gmail.com"); // smtp usernmae
+define("MAIL_PASSWORD", "teamc.cmsc207"); // smtp password
+define("MAIL_HOST", "smtp.gmail.com"); // smtp host
+define("MAILER", "smtp");
+
+define("SENDER_NAME", "Admin");
+define("SERDER_EMAIL", "cmsc207.teamc@gmail.com");
+?>
